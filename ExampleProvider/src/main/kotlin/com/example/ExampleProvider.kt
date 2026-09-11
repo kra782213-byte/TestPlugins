0cc4cbfc7c4ff91434daf18ee9d531cb98f824ef
kra782213-byte <kra782213@gmail.com>
2026-09-11 10:27:50 +0300

    Add HDFilmCehennemiProvider class with functionality

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,81 @@
 package com.example
 
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.nodes.Element
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+class HDFilmCehennemiProvider : MainAPI() {
+    override var mainUrl = "https://www.hdfilmcehennemi.nl"
+    override var name = "HDFilmCehennemi"
+    override val hasMainPage = true
+    override var lang = "tr"
+    override val hasDownloadSupport = true
     override val supportedTypes = setOf(TvType.Movie)
 
-    override var lang = "en"
+    override val mainPage = mainPageOf(
+        "$mainUrl/page/" to "Yeni Filmler"
+    )
 
-    // Enable this when your provider has a main page
-    override val hasMainPage = true
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val url = request.data + page
+        val document = app.get(url).document
+        val home = ArrayList<SearchResponse>()
 
-    // This function gets called when you search for something
+        document.select("div.card-body.poster-card-body").forEach { element ->
+            val title = element.selectFirst("h2.title")?.text() ?: ""
+            val link = element.selectFirst("a")?.attr("href") ?: ""
+            val posterUrl = element.selectFirst("img")?.attr("data-src") ?: ""
+
+            if (title.isNotBlank() && link.isNotBlank()) {
+                home.add(newMovieSearchResponse(title, link, TvType.Movie) {
+                    this.posterUrl = posterUrl
+                })
+            }
+        }
+
+        return newHomePageResponse(request.name, home)
+    }
+
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+        val searchUrl = "$mainUrl/arama/$query"
+        val document = app.get(searchUrl).document
+        val searchResults = ArrayList<SearchResponse>()
+
+        document.select("div.card-body.poster-card-body").forEach { element ->
+            val title = element.selectFirst("h2.title")?.text() ?: ""
+            val link = element.selectFirst("a")?.attr("href") ?: ""
+            val posterUrl = element.selectFirst("img")?.attr("data-src") ?: ""
+
+            if (title.isNotBlank() && link.isNotBlank()) {
+                searchResults.add(newMovieSearchResponse(title, link, TvType.Movie) {
+                    this.posterUrl = posterUrl
+                })
+            }
+        }
+        return searchResults
+    }
+
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+        val title = document.selectFirst("h1.title")?.text() ?: "Bilinmeyen Film"
+        val posterUrl = document.selectFirst("img.poster")?.attr("src")
+        val plot = document.selectFirst("div.summary")?.text()
+
+        return newMovieLoadResponse(title, url, TvType.Movie, url) {
+            this.posterUrl = posterUrl
+            this.plot = plot
+        }
+    }
+
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        return true
     }
 }
